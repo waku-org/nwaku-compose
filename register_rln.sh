1763fb7e5dc32403a0e18bed300cc210356c5b1e
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2024-01-10 14:54:01 +0530

    chore: address review comments

--- register_rln.sh
+++ register_rln.sh
@@ -20,5 +20,5 @@ docker run -v $(pwd)/keystore:/keystore/:Z wakuorg/nwaku:v0.21.3 generateRlnKeys
 --rln-relay-eth-private-key=${ETH_TESTNET_KEY} \
 --rln-relay-eth-contract-address=0xF471d71E9b1455bBF4b85d475afb9BB0954A29c4 \
 --rln-relay-cred-path=/keystore/keystore.json \
---rln-relay-cred-password="'"${RLN_RELAY_CRED_PASSWORD}"'" \
+--rln-relay-cred-password="${RLN_RELAY_CRED_PASSWORD}" \
 --execute
