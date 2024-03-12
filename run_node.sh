f269a8ed2695ef1cdc763891dc651093820f2fe3
NagyZoltanPeter <113987313+NagyZoltanPeter@users.noreply.github.com>
2024-03-12 09:44:21 +0100

    chore: Upgrade nwaku image to v0.26.0 (#74)

--- run_node.sh
+++ run_node.sh
@@ -84,6 +84,7 @@ exec /usr/bin/wakunode\
   --rest-admin=true\
   --rest-address=0.0.0.0\
   --rest-port=8645\
+  --rest-allow-origin="waku-org.github.io"\
   --nat=extip:"${MY_EXT_IP}"\
   --store=true\
   --store-message-db-url="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/postgres"\
