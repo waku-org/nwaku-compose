6b3718586ca90ee1011dcbc1c6d0deea5fc6737b
Alvaro Revuelta <alvrevuelta@gmail.com>
2024-03-14 11:28:47 +0100

    Cleanup flags (#76)

--- run_node.sh
+++ run_node.sh
@@ -61,22 +61,11 @@ fi
 
 exec /usr/bin/wakunode\
   --relay=true\
-  --pubsub-topic=/waku/2/rs/1/0\
-  --pubsub-topic=/waku/2/rs/1/1\
-  --pubsub-topic=/waku/2/rs/1/2\
-  --pubsub-topic=/waku/2/rs/1/3\
-  --pubsub-topic=/waku/2/rs/1/4\
-  --pubsub-topic=/waku/2/rs/1/5\
-  --pubsub-topic=/waku/2/rs/1/6\
-  --pubsub-topic=/waku/2/rs/1/7\
   --filter=true\
   --lightpush=true\
   --keep-alive=true\
   --max-connections=150\
   --cluster-id=1\
-  --discv5-bootstrap-node="enr:-QESuEC1p_s3xJzAC_XlOuuNrhVUETmfhbm1wxRGis0f7DlqGSw2FM-p2Ugl_r25UHQJ3f1rIRrpzxJXSMaJe4yk1XFSAYJpZIJ2NIJpcISygI2rim11bHRpYWRkcnO4XAArNiZub2RlLTAxLmRvLWFtczMud2FrdS50ZXN0LnN0YXR1c2ltLm5ldAZ2XwAtNiZub2RlLTAxLmRvLWFtczMud2FrdS50ZXN0LnN0YXR1c2ltLm5ldAYfQN4DgnJzkwABCAAAAAEAAgADAAQABQAGAAeJc2VjcDI1NmsxoQJATXRSRSUyTw_QLB6H_U3oziVQgNRgrXpK7wp2AMyNxYN0Y3CCdl-DdWRwgiMohXdha3UyDw"\
-  --discv5-bootstrap-node="enr:-QEkuECnZ3IbVAgkOzv-QLnKC4dRKAPRY80m1-R7G8jZ7yfT3ipEfBrhKN7ARcQgQ-vg-h40AQzyvAkPYlHPaFKk6u9uAYJpZIJ2NIJpcIQiEAFDim11bHRpYWRkcnO4bgA0Ni9ub2RlLTAxLmdjLXVzLWNlbnRyYWwxLWEud2FrdS50ZXN0LnN0YXR1c2ltLm5ldAZ2XwA2Ni9ub2RlLTAxLmdjLXVzLWNlbnRyYWwxLWEud2FrdS50ZXN0LnN0YXR1c2ltLm5ldAYfQN4DgnJzkwABCAAAAAEAAgADAAQABQAGAAeJc2VjcDI1NmsxoQMIJwesBVgUiBCi8yiXGx7RWylBQkYm1U9dvEy-neLG2YN0Y3CCdl-DdWRwgiMohXdha3UyDw"\
-  --discv5-bootstrap-node="enr:-QEkuEDzQyIAhs-CgBHIrJqtBv3EY1uP1Psrc-y8yJKsmxW7dh3DNcq2ergMUWSFVcJNlfcgBeVsFPkgd_QopRIiCV2pAYJpZIJ2NIJpcIQI2ttrim11bHRpYWRkcnO4bgA0Ni9ub2RlLTAxLmFjLWNuLWhvbmdrb25nLWMud2FrdS50ZXN0LnN0YXR1c2ltLm5ldAZ2XwA2Ni9ub2RlLTAxLmFjLWNuLWhvbmdrb25nLWMud2FrdS50ZXN0LnN0YXR1c2ltLm5ldAYfQN4DgnJzkwABCAAAAAEAAgADAAQABQAGAAeJc2VjcDI1NmsxoQJIN4qwz3v4r2Q8Bv8zZD0eqBcKw6bdLvdkV7-JLjqIj4N0Y3CCdl-DdWRwgiMohXdha3UyDw"\
   --discv5-discovery=true\
   --discv5-udp-port=9005\
   --discv5-enr-auto-update=True\
@@ -93,9 +82,6 @@ exec /usr/bin/wakunode\
   --nat=extip:"${MY_EXT_IP}"\
   --store=true\
   --store-message-db-url="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/postgres"\
-  --rln-relay=true\
-  --rln-relay-dynamic=true\
-  --rln-relay-eth-contract-address="${RLN_RELAY_CONTRACT_ADDRESS}"\
   --rln-relay-eth-client-address="${ETH_CLIENT_ADDRESS}"\
   --rln-relay-tree-path="/etc/rln_tree"\
   ${RLN_RELAY_CRED_PATH}\
