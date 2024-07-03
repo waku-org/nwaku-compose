33cf40c5821a54178247a4e78c302bdd4c21a572
Alvaro Revuelta <alvrevuelta@gmail.com>
2024-07-03 15:57:04 +0200

    Fix image (temporal)

--- register_rln.sh
+++ register_rln.sh
@@ -21,10 +21,10 @@ if test -n "${ETH_CLIENT_ADDRESS}"; then
   exit 1
 fi
 
-docker run -v $(pwd)/keystore:/keystore/:Z harbor.status.im/wakuorg/nwaku:v0.30.0 generateRlnKeystore \
+docker run -v $(pwd)/keystore:/keystore/:Z quay.io/wakuorg/nwaku-pr:2872 generateRlnKeystore \
 --rln-relay-eth-client-address=${RLN_RELAY_ETH_CLIENT_ADDRESS} \
 --rln-relay-eth-private-key=${ETH_TESTNET_KEY} \
---rln-relay-eth-contract-address=0x4976Df0f61135EF3E5720D92eadE2e5F47A68Ef9 \
+--rln-relay-eth-contract-address=0xCB33Aa5B38d79E3D9Fa8B10afF38AA201399a7e3 \
 --rln-relay-cred-path=/keystore/keystore.json \
 --rln-relay-cred-password="${RLN_RELAY_CRED_PASSWORD}" \
 --rln-relay-user-message-limit=20 \
